9469111771f60c5b734979c35f29558001c9d9a9
RainerMauersberger23 <829532@mystma.org>
2021-02-12 00:10:52 +0000

    Unit Test 3

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#WRITE YOUR CODE IN THIS FILE
+def countB(w):
+    count = 0
+    for i in range (0, len(w)):
+        if w[i] == "b":
+            count = (count + 1)
+    return count
+
+
+
+print (countB("babble"))
+
+def removeLast():
+
+def sumBetweenOdd():
+
+def firstLast():
+
+def firstLast():
+
+
